1c3cfaa44644582b024c092835a730dcf0664fc5
ebnyame <ebnyame001@st.ug.edu.gh>
2025-04-03 11:42:47 +0000

    Basic Strings

---
+++ Basic_Strings/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("--------------------------------------------Windows Path--------------------------------------------")
+	windowsPath()
+	fmt.Println()
+	fmt.Println()
+
+	fmt.Println("--------------------------------------------Print Json--------------------------------------------")
+	printJson()
+	fmt.Println()
+	fmt.Println()
+
+	fmt.Println("--------------------------------------------Raw Concatenate--------------------------------------------")
+	rawConcate()
+	fmt.Println()
+	fmt.Println()
+}
--- Playgroundd/main.go
+++ Playgroundd/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
-
-var Args []string
 
 func main() {
 
-	fmt.Printf("%#v\n", os.Args)
+	// fmt.Printf("%#v\n", os.Args)
+	msg := os.Args[1]
+	l := len(msg)
+	// ed := msg + strings.Repeat("!", l)
+	rS := strings.Repeat("!", l)
+
+	fmt.Println(rS + " " + msg + " " + rS)
+
 }
