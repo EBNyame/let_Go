b67d2e59663b26a2e840ee5d03ad175aee3bb72b
ebnyame <ebnyame001@st.ug.edu.gh>
2025-04-22 09:58:00 +0000

    slices

--- MapsArrayysSLices/main.go
+++ MapsArrayysSLices/main.go
@@ -6,6 +6,9 @@ import "fmt"
 
 func main(){
 	array()
+	slice()
+	slicingArray()
+	addingToSlice()
 }
 
 
@@ -14,4 +17,27 @@ func array(){
 	var x = [2]int{3, 6}
 	x[0] = 5
 	fmt.Println(x)
+}
+
+
+// ............................Slices................................
+func slice(){
+	var x = make([]int, 3)
+	fmt.Println(x)
+}
+
+func slicingArray(){
+	x := [5]int{3, 5, 6, 8, 9}
+	y := x[1:5]
+	fmt.Println(y)
+}
+
+func addingToSlice(){
+	x := [] int{1, 3}
+	x = append(x, 4,5,6,7)
+	fmt.Println(x)
+
+	for index, value := range x{
+		fmt.Printf("Index:  %d, Value: %d\n", index, value)
+	}
 }
